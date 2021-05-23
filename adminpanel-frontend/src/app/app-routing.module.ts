10267cdb3c15efc43976346473dc7db998bce364
Ariz Hernández <arishassam@gmail.com>
2021-05-22 19:46:38 -0600

    primer commit

--- adminpanel-frontend/src/app/app-routing.module.ts
+++ adminpanel-frontend/src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { SingUpComponent } from './auth/sing-up/sing-up.component';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
-  {path: 'signup', component: SingUpComponent},
-  {path: '**', redirectTo: 'login'}
+
 ];
 
 @NgModule({
